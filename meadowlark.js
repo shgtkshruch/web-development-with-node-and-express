19364bae52c101a8db33d9002727e570b716dc83
shgtkshruch <shgtk.shruch@gmail.com>
2014-10-26 09:24:30 +0900

    tempalte engine handlebars

--- meadowlark.js
+++ meadowlark.js
@@ -2,31 +2,34 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+  res.render('home');
 });
 
 app.get('/about', function (req, res) {
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
+  res.render('about');
 });
 
 // custome 404 page
 app.use(function (req, res) {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 // custome 500 page
 app.use(function (err, req, res, next) {
   console.error(err.stack);  
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function () {
