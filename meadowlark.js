4d0c2c338029c413f79a66f1234aefdd4cf57b1a
shgtkshruch <shgtk.shruch@gmail.com>
2014-10-28 07:50:31 +0900

    Add newsletter sign-up form to use AJAX.

--- meadowlark.js
+++ meadowlark.js
@@ -121,11 +121,15 @@ app.get('/newsletter', function (req, res) {
 });
 
 app.post('/process', function (req, res) {
-  console.log('Form (from querystring): ' + req.query.form);
-  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-  console.log('Name (from visible form field): ' + req.body.name);
-  console.log('Email (from visible form field): ' + req.body.email);
-  res.redirect(303, '/thank-you');
+  if (req.xhr || req.accepts('json, html') === 'json') {
+    res.send({success: true});
+  } else {
+    res.redirect(303, '/thank-you');
+  }
+});
+
+app.get('/thank-you', function (req, res) {
+  res.render('thank-you');
 });
 
 // custome 404 page
